4d101ee2db21d4cbcbb03944fe06c3feca8d902a
Manuel Pimenta <CTW03345@criticaltechworks.com>
2025-09-02 10:30:51 +0100

    recovery of wrongfully deleted src folder in devlog

---
+++ devlog/src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Scout Toolkit Devlog';
+export const SITE_DESCRIPTION = 'Building tools to help scout leaders bridge the gap between kids\' needs and their knowledge';
